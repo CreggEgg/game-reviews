79530c4b2cb625e11119096bb3e60128c5982a8e
BuildTools <unconfigured@null.spigotmc.org>
2022-12-31 14:21:38 -0500

    pfp appears in top right corner

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,20 +1,42 @@
 <script lang="ts">
-	import TopAppBar, { Row } from "@smui/top-app-bar";
-	import Tab, { Label } from "@smui/tab";
-	import TabBar from "@smui/tab-bar";
+	import TopAppBar, { Row, Section, Title } from "@smui/top-app-bar";
+	import Button from "@smui/button";
+	import { profileData, type ProfileData } from "./oauth/oauth";
 
+	let profile = null as ProfileData;
+	let [username, profilePicture]: string[] = ["", ""];
+	profileData.subscribe((value) => {
+		profile = value;
+	});
+	let signed_in = false;
+	if (profile !== null) {
+		signed_in = true;
+		username = profile.username;
+		profilePicture = profile.profilePicture;
+	}
 	let active: string;
 </script>
 
-<TopAppBar variant="static" color="secondary">
-	<Row
-		><TabBar tabs={["Home", "Merchandise", "About Us"]} let:tab bind:active>
-			<!-- Note: the `tab` property is required! -->
-			<Tab {tab} href="">
-				<Label>{tab}</Label>
-			</Tab>
-		</TabBar></Row
-	>
+<TopAppBar style="margin-bottom: 2vh;" variant="static" color="primary">
+	<Row>
+		<Section>
+			<Title><Button href="/">Game reviews</Button></Title>
+		</Section>
+		{#if signed_in}
+			<Section align="end" toolbar>
+				<Title>{username}</Title>
+				<img id="pfp" src={profilePicture} alt="user profile" />
+			</Section>
+		{/if}
+	</Row>
 </TopAppBar>
 
 <slot />
+
+<style>
+	#pfp {
+		height: 100%;
+		margin-left: 2vw;
+		border-radius: 100px;
+	}
+</style>
--- src/routes/oauth/oauth.ts
+++ src/routes/oauth/oauth.ts
@@ -1,24 +1,30 @@
-
-import {writable, type Writable} from 'svelte/store';
-import {browser} from "$app/environment";
-
+import { writable, type Writable } from "svelte/store";
+import { browser } from "$app/environment";
 
 function fromLocalStorage(storageKey: string, fallback: any) {
-	if(browser) {
+	if (browser) {
 		const storedValue = window.localStorage.getItem(storageKey);
-		if(storedValue !== undefined) return (typeof fallback === 'object' && storedValue !== undefined) ? JSON.parse(storedValue!) : storedValue
+		if (storedValue !== undefined)
+			return typeof fallback === "object" && storedValue !== undefined
+				? JSON.parse(storedValue!)
+				: storedValue;
 	}
 	return fallback;
 }
 
 function toLocalStorage<T>(store: Writable<T>, storageKey: string) {
-	if(browser) {
-		store.subscribe(value => {const storageValue = JSON.stringify(value); window.localStorage.setItem(storageKey, storageValue)})
+	if (browser) {
+		store.subscribe((value) => {
+			const storageValue = JSON.stringify(value);
+			window.localStorage.setItem(storageKey, storageValue);
+		});
 	}
 }
 
-export const profileData = writable<ProfileData>(fromLocalStorage("profileData", null));
-toLocalStorage<ProfileData>(profileData, "profileData")
+export const profileData = writable<ProfileData>(
+	fromLocalStorage("profileData", null)
+);
+toLocalStorage<ProfileData>(profileData, "profileData");
 
 export type ProfileData = {
 	profilePicture: string;
