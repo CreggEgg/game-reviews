23a66c8559439611a8608d38728baa1876e52da0
BuildTools <unconfigured@null.spigotmc.org>
2022-12-31 11:24:36 -0500

    Added app bar

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import TopAppBar, { Row } from "@smui/top-app-bar";
+	import Tab, { Label } from "@smui/tab";
+	import TabBar from "@smui/tab-bar";
+
+	let active: string;
+</script>
+
+<TopAppBar variant="static" color="secondary">
+	<Row
+		><TabBar tabs={["Home", "Merchandise", "About Us"]} let:tab bind:active>
+			<!-- Note: the `tab` property is required! -->
+			<Tab {tab} href="">
+				<Label>{tab}</Label>
+			</Tab>
+		</TabBar></Row
+	>
+</TopAppBar>
+
+<slot />
