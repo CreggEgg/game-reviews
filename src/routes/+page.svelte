c8ffa17c2fcb043572bb4bede8ac6d34e8d20d32
BuildTools <unconfigured@null.spigotmc.org>
2023-01-02 17:09:47 -0500

    Get started dialog no longer appears if you are already signed in

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,33 +1,41 @@
 <script lang="ts">
 	import Button, { Label } from "@smui/button";
 	import Dialog, { Title, Content, Actions, InitialFocus } from "@smui/dialog";
+	import { profileData, type ProfileData } from "./oauth/oauth";
 	let get_signed_up_dialog = true;
+	let currentProfileData: ProfileData = null;
+
+	profileData.subscribe((value) => (currentProfileData = value));
 </script>
 
 <body>
-	<Dialog
-		bind:open={get_signed_up_dialog}
-		aria-labelledby="sign-up-dialog-title"
-		aria-describedby="sign-up-dialog-content"
-	>
-		<!-- Title cannot contain leading whitespace due to mdc-typography-baseline-top() -->
-		<Title id="sign-up-dialog-title">Get started on Epic Gamer</Title>
-		<Content id="sign-up-dialog-content"
-			>Sign in to get personalized game reviews or just browse the reviews of
-			our users.</Content
+	{#if currentProfileData === null || currentProfileData === undefined}
+		<Dialog
+			bind:open={get_signed_up_dialog}
+			aria-labelledby="sign-up-dialog-title"
+			aria-describedby="sign-up-dialog-content"
 		>
-		<Actions>
-			<Button variant="unelevated" href="/oauth" use={[InitialFocus]}
-				>Get started</Button
+			<!-- Title cannot contain leading whitespace due to mdc-typography-baseline-top() -->
+			<Title id="sign-up-dialog-title">Get started on Epic Gamer</Title>
+			<Content id="sign-up-dialog-content"
+				>Sign in to get personalized game reviews or just browse the reviews of
+				our users.</Content
 			>
-			<Button
-				on:click={() => (get_signed_up_dialog = false)}
-				variant="outlined"
-			>
-				<Label>I'll just browse</Label>
-			</Button>
-		</Actions>
-	</Dialog>
-	<h1>Hello world</h1>
-	<Button variant="unelevated" href="/oauth">Get started</Button>
+			<Actions>
+				<Button variant="unelevated" href="/oauth" use={[InitialFocus]}
+					>Get started</Button
+				>
+				<Button
+					on:click={() => (get_signed_up_dialog = false)}
+					variant="outlined"
+				>
+					<Label>I'll just browse</Label>
+				</Button>
+			</Actions>
+		</Dialog>
+	{/if}
+
+	{#if currentProfileData === null || currentProfileData === undefined}
+		<Button variant="unelevated" href="/oauth">Get started</Button>
+	{/if}
 </body>
